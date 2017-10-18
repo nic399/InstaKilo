6dbdb98d73e6f420369a5eb2ccb40b1ebf53200f
Nicholas Fung <nbfung@sfu.ca>
2017-10-18 10:02:09 -0700

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Nicholas Fung on 2017-10-18.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
